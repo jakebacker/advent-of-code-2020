de9c2cc867d97c7c0d4a6197f67f9c5927cab3f3
jakebacker <jbacker42@gmail.com>
2020-12-19 13:44:45 -0500

    Day 19

--- day19.py
+++ day19.py
@@ -1,77 +1,33 @@
+import re
+
 data = []
 
 with open("inputs/day19.txt", "r") as f:
 #with open("test/day19.txt", "r") as f:
     data = f.read().split("\n")  # This part may change
 
-
-class MatchRule:
-
-    left = []  # Array of MatchRule or str
-    right = None
-
-    def __init__(self):
-        self.left = []
-
-    def add_left(self, left):
-        self.left.append(left)
-
-    def add_right(self, right):
-        if self.right is None:
-            self.right = []
-        self.right.append(right)
+rules = []
 
 
-def matches(val, rule: MatchRule):
-    curr_val = val
-    if rule.right is None:
-        for r in rule.left:
-            if isinstance(r, str):
-                if curr_val.startswith(r):
-                    curr_val = curr_val[curr_val.index(r) + len(r):]
-                else:
-                    return False, ""
-            else:
-                result = matches(curr_val, r)
-                if not result[0]:
-                    return False, ""
-                curr_val = result[1]
-        return True, curr_val
+# This would work, but max recursion depth problems
+def expr_gen(rule):
+    if "\"" in rule:
+        return rule[1:len(rule)-1]
+    elif "|" in rule:
+        left = rule.split("|")[0].strip()
+        right = rule.split("|")[1].strip()
+        left_expr = expr_gen(left)
+        right_expr = expr_gen(right)
+
+        return "(" + left_expr + "|" + right_expr + ")"
     else:
-        matches_left = True
-        matches_right = True
-        for r in rule.left:
-            if isinstance(r, str):
-                if curr_val.startswith(r):
-                    curr_val = curr_val[curr_val.index(r) + len(r):]
-                else:
-                    matches_left = False
-                    break
-            else:
-                result = matches(curr_val, r)
-                if not result[0]:
-                    matches_left = False
-                    break
-                curr_val = result[1]
-        if matches_left:
-            return True, curr_val
-        curr_val = val
-        for r in rule.right:
-            if isinstance(r, str):
-                if curr_val.startswith(r):
-                    curr_val = curr_val[curr_val.index(r) + len(r):]
-                else:
-                    matches_right = False
-                    break
-            else:
-                result = matches(curr_val, r)
-                if not result[0]:
-                    matches_right = False
-                    break
-                curr_val = result[1]
-        if matches_right:
-            return True, curr_val
-        return False, ""
+        expr = ""
+        parts = rule.split(" ")
+        for r in parts:
+            if r.strip() == "":
+                continue
+            expr += expr_gen(rules[int(r)])
+        return expr
 
 
 rules_str = []
@@ -83,41 +39,68 @@ for i in range(0, len(data)):
         messages = data[i+1:]
         break
 
-rules = []
-
-for i in range(0, len(rules_str)):
-    rules.append(MatchRule())
+rules = [""]*len(rules_str)
 
 for i in range(0, len(rules_str)):
     rule_num = int(rules_str[i].split(":")[0].strip())
     r = rules_str[i].split(":")[1].strip()
 
-    if "|" in r:
-        left = r.split("|")[0].strip()
-        right = r.split("|")[1].strip()
+    rules[rule_num] = r
 
-        for l in left:
-            if not l.strip() == "":
-                rules[rule_num].add_left(rules[int(l)])
-        for r in right:
-            if not r.strip() == "":
-                rules[rule_num].add_right(rules[int(r)])
-    elif "\"" in r:
-        rules[rule_num].add_left(r[1:len(r)-1])
-    else:
-        for l in r:
-            if not l.strip() == "":
-                rules[rule_num].add_left(rules[int(l)])
 
 count = 0
-for m in messages:
-    result = matches(m, rules[0])
 
-    if result[0] and result[1].strip() == "":
-        print(matches(m, rules[0]))
+expr = re.compile("^" + expr_gen(rules[0]))
+
+for m in messages:
+    result = expr.fullmatch(m)
+
+    if result is not None:
         count += 1
+        print(m)
 print(count)
 
 
 # REGEX: ^a(ab|ba)
 # ^a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b
+
+#  Pt2
+'''
+with open("test/day19pt2.txt", "r") as f:
+    data = f.read().split("\n")  # This part may change
+
+for i in range(0, len(data)):
+    if data[i].strip() == "":
+        rules_str = data[:i]
+        messages = data[i+1:]
+        break
+
+rules = [""]*150
+
+for i in range(0, len(rules_str)):
+    rule_num = int(rules_str[i].split(":")[0].strip())
+    r = rules_str[i].split(":")[1].strip()
+
+    rules[rule_num] = r
+'''
+rules[8] = "42 | 42 8"
+rules[11] = "42 31 | 42 11 31"
+
+expr_42 = expr_gen(rules[42])
+expr_31 = expr_gen(rules[31])
+
+expr_8 = "(" + expr_42 + ")+"
+
+
+count = 0
+for i in range(1, 500):
+    expr_11 = "(" + expr_42 + "){" + str(i) + "}(" + expr_31 + "){" + str(i) + "}"
+
+    expr_0 = re.compile("^" + expr_8 + expr_11)
+
+    for m in messages:
+        result = expr_0.fullmatch(m)
+
+        if result is not None:
+            count += 1
+print(count)
